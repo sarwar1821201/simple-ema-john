dc209d1525ea74529fe4f7975fd849b8ff421ba0
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-09-21 13:22:10 +0600

    fetch the data by useeffect

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    
+    const [products, setProducts]= useState([]);
+
+    useEffect( ()=>{
+      
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data=> setProducts(data))
+
+    } , [])
+
+    return (
+        <div className='shop-container'>
+
+     <div className="products-container">
+
+             <h2>Products Coming Here: {products.length}</h2> 
+
+            </div>
+
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+                <p>Selected Items: </p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
