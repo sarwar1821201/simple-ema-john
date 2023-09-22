5f578000c3f4c2c30e4dc17d74f21a0eef12719f
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-09-23 00:04:32 +0600

    set the quantity

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -11,8 +11,16 @@ const Cart = ({cart}) => {
 
 
     for (let product of cart) {
-         totalPrice=totalPrice+ product.price;
+          
+         // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+
+
+         totalPrice=totalPrice+ product.price * product.quantity;
          totalShipping = totalShipping + product.shipping;
+         quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -22,7 +30,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length} </p>
+                <p>Selected Items: {quantity} </p>
                 <p>Total Price:$ {totalPrice}  </p>
             <p>Shipping:${totalShipping} </p>
             <p>Tax: $ {tax.toFixed(2)}</p>
