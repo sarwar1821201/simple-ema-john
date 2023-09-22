dbd4b4aefa4fe1573cdca25a29afd9f7474a5277
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-09-22 10:35:26 +0600

    calculate order summary

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     
@@ -41,8 +42,7 @@ const Shop = () => {
             </div>
 
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length} </p>
+                <Cart cart={cart}></Cart>
             </div>
             
         </div>
