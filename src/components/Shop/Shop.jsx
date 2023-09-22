0ca017bde67563fd48ab5dc833ee7f39a0b5132c
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-09-22 23:19:15 +0600

    retrive product from stored cart and save it to cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -19,10 +19,27 @@ const Shop = () => {
 
     useEffect( ()=>{
 
+    
        const storedCart= getShoppingCart();
-       
+       const savedCart = [];
 
-    } ,[])
+       
+       // step 1: get id of the addedProduct
+       for(const id in storedCart){
+        // step 2: get product from products state by using id
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+            // step 3: add quantity
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            // step 4: add the added product to the saved cart
+            savedCart.push(addedProduct);
+        }
+       
+       }
+       setCart(savedCart)
+
+    },[products]);
 
 
     const handleAddToCart = (product) => {
