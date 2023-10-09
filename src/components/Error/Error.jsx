b18d826b0de0a3ed8f1d4a50b8d773d5d0f77a3f
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-09 21:42:05 +0600

    get error message from error route

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
+    const {error,status}= useRouteError()
     return (
         <div>
-            <h3>NO Data Found !!!!  Please Try Again</h3>
+            <h1>NO Data Found !!!!  Please Try Again.</h1>
+            <h2>Error Comes From : {error.message}  </h2>
         </div>
     );
 };
