4e28670e8db63f078511f2df7bab6f68abdd19b6
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-27 21:13:07 +0600

    use a loading functionality by using navigation

--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const Home = () => {
+
+   const navigation= useNavigation();
+
     return (
         <div>
             <Header></Header>
+
+          <div>
+            {
+              navigation.state=== 'loading' ? 'Loading....' : ' '
+            }
+          </div>
+
             <Outlet></Outlet>
         </div>
     );
