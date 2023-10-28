cd7ba69ccdc9a5e67bc30f2732581e062a4c49a1
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-28 11:56:52 +0600

    host react app to firebase

--- src/__steps.js
+++ src/__steps.js
@@ -22,3 +22,33 @@
 
 
 */
+
+
+///// Hosting 
+
+/*
+  one time per pc
+
+1. npm install -g  firebase-tools
+2. firebase login (one time for one pc)
+
+
+-------------------
+ for each project one time
+ 1. firebase init
+ 2. proceed (yes)
+ 3. hosting: firebase (up and down arrow)  use space bar to select
+ 4. existing project
+ 5. select the project carefully
+ 6. which project as public directory: dist
+ 7. single page application : yes
+ 8. continues deployment : no
+
+
+ --------
+ for every time deploy
+
+ 1. npm run build
+ 2. firebase deploy
+
+*/
